18f99e3e94d2d9d47b09bc9734f38e71a60e7895
Ulises Ortega <ulises.ortega2937@alumnos.udg.mx>
2022-02-16 17:41:17 -0600

    Añadidas vistas de prueba

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use phpDocumentor\Reflection\Types\Null_;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,30 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/hola-mundo', function () {
+    return view('paginas.hola-mundo');
+});
+
+// Route::get('/grabaciones', function () {
+//     return view('paginas.grabaciones');
+// });
+
+// Se espera que se reciba una variable, en este caso
+// nombre
+Route::get('/grabaciones/{nombre}', function ($nombre) {
+    // Se muere la aplicación y se retorna lo que se pasa
+    // como parántro
+    // dd($nombre);
+    // Se pasa el parámetro a la vista a través del método
+    // compact(), se pasa como string
+    return view('paginas.grabaciones', compact('nombre'));
+    // Hace lo mismo que la línea anterior, pero más verbose
+    //return view('paginas.grabaciones')->with(['nombre' => $nombre]);
+});
+
+//  Con el ? se indica que es un parámetro opcional
+Route::get('/grabaciones/{nombre}/{anio?}', function ($nombre, $anio = NULL) {
+    return view('paginas.grabaciones', compact('nombre', 'anio'));
 });
